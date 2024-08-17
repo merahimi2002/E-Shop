f2f8108c68562fa700090f23ce35b0aff243f431
MErahimi <mohammaderahimi80@gmail.com>
2024-08-17 20:19:21 +0330

    Add changes to upload page

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,16 +1,39 @@
 "use client";
 
-import { CldUploadWidget } from "next-cloudinary";
+import { CldUploadWidget, CldImage } from "next-cloudinary";
+import { useState } from "react";
+
+interface CloudinaryResult {
+  public_id: string;
+}
 
 const UploadPage = () => {
+  const [publicId, setPublicId] = useState("");
   return (
-    <CldUploadWidget uploadPreset="DBimage">
-      {({ open }) => (
-        <button onClick={() => open()} className="btn btn-primary mt-5">
-          Upload
-        </button>
+    <>
+      {publicId && (
+        <CldImage src={publicId} alt="pic" width={270} height={180} />
       )}
-    </CldUploadWidget>
+      <CldUploadWidget
+        uploadPreset="DBimage"
+        options={{
+          sources: ["local"],
+          multiple: false,
+          maxFiles:2,
+          maxFileSize: 1000000
+        }}
+        onSuccess={(result, { widget }) => {
+          const info = result.info as CloudinaryResult;
+          setPublicId(info.public_id);
+        }}
+      >
+        {({ open }) => (
+          <button onClick={() => open()} className="btn btn-primary mt-5">
+            Upload
+          </button>
+        )}
+      </CldUploadWidget>
+    </>
   );
 };
 

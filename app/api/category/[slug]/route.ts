11c6e5496cf2c17c086bf0c77a6bd0f06a6e82ea
MErahimi <mohammaderahimi80@gmail.com>
2024-08-26 20:14:49 +0330

    Add Category patch API

---
+++ app/api/category/[slug]/route.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from "next/server";
+import { CategorySchema } from "../../validation/validationSchema";
+import prisma from "@/prisma/client";
+
+export async function PATCH(
+  request: NextRequest,
+  { params }: { params: { slug: string } }
+) {
+  const body = await request.json();
+  const validation = CategorySchema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.format(), { status: 400 });
+
+  const Category = await prisma.category.findMany({
+    where: { slug: params.slug },
+  });
+
+  if (!Category)
+    return NextResponse.json({ error: "Invalid Category" }, { status: 404 });
+
+  const updatedCategory = await prisma.category.update({
+    where: { slug: params.slug },
+    data: {
+      title: body.title,
+      imageUrl: body.imageUrl,
+    },
+  });
+
+  return NextResponse.json(updatedCategory);
+}

b34d76bbb5e7592dccdd1b7d7a151b258c2453d9
MErahimi <mohammaderahimi80@gmail.com>
2024-08-30 19:59:05 +0330

    fix bug

--- app/api/validation/validationSchema.ts
+++ app/api/validation/validationSchema.ts
@@ -1,7 +1,16 @@
 import { z } from "zod";
 
+const forbiddenCharactersRegex = /[\/\?:@&=\+\$_.!~*'()#]/;
+
 export const ProductSchema = z.object({
-  title: z.string().min(1, "Title is required").max(50),
+  title: z
+    .string()
+    .min(1, "Title is required")
+    .max(50)
+    .refine((value) => !forbiddenCharactersRegex.test(value), {
+      message:
+        "you cannot used this characters: / ? : @ & = + $ _ . ! ~ * ' ( ) #",
+    }),
   slug: z.string().min(1, "Slug is required").max(50),
   description: z.string().min(1, "Description is required"),
   imageUrl: z.string().min(1, "Image is required").max(255),
@@ -10,7 +19,14 @@ export const ProductSchema = z.object({
 });
 
 export const CategorySchema = z.object({
-  title: z.string().min(1, "Title is required").max(50),
+  title: z
+    .string()
+    .min(1, "Title is required")
+    .max(50)
+    .refine((value) => !forbiddenCharactersRegex.test(value), {
+      message:
+        "you cannot used this characters: / ? : @ & = + $ _ . ! ~ * ' ( ) #",
+    }),
   slug: z.string().min(1, "Slug is required").max(50),
   imageUrl: z.string().min(1, "Image is required").max(255),
 });

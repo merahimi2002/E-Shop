7ceae71b204ef49ae761763f92a23fd80fd77e89
MErahimi <mohammaderahimi80@gmail.com>
2024-09-04 13:58:00 +0330

    Create 404 error

---
+++ app/not-found.tsx
@@ -0,0 +1,26 @@
+import image from "@/public/image/404.png";
+import Image from "next/image";
+
+const NotFoundPage = () => {
+  return (
+    <section>
+      <div className="container">
+        <div className="grid grid-cols-1 md:grid-cols-2">
+          <div className="flex flex-col flex-center">
+            <h2 className="text-9xl text-primary font-semibold">404</h2>
+            <p className="text-4xl text-primary font-medium mt-5">page not found</p>
+          </div>
+          <Image
+            src={image}
+            alt="Logo"
+            priority
+            className="mt-5"
+            style={{ objectFit: "contain", width: "100%", height: "70vh" }}
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default NotFoundPage;

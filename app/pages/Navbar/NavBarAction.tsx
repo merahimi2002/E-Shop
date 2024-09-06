85d79fe088e955b98366d87c70fb50e6ea094f25
MErahimi <mohammaderahimi80@gmail.com>
2024-09-06 17:56:30 +0330

    add style to navbar login

--- app/pages/Navbar/NavBarAction.tsx
+++ app/pages/Navbar/NavBarAction.tsx
@@ -2,7 +2,7 @@
 
 import { useSession } from "next-auth/react";
 import { RiAdminLine } from "react-icons/ri";
-import { TbLogout, TbLogin2 } from "react-icons/tb";
+import { TbLogin2, TbLogout } from "react-icons/tb";
 import Link from "next/link";
 import ThemeController from "./ThemeController";
 
@@ -19,9 +19,36 @@ const NavBarAction = () => {
           <span className="loading loading-spinner loading-md text-white"></span>
         )}
         {status === "authenticated" && (
-          <Link href="/api/auth/signout">
-            <TbLogout className="text-white h-8 w-8" />
-          </Link>
+          <div className="dropdown dropdown-end z-50">
+            <div tabIndex={0} role="button">
+              {session.user?.image ? (
+                <div className="avatar mt-2">
+                  <div className="ring-primary ring-offset-white w-8 rounded-full ring ring-offset-2">
+                    <img src={session.user.image} />
+                  </div>
+                </div>
+              ) : (
+                <div className="avatar placeholder border-2 bg-[#3b814f] rounded-full flex flex-center">
+                  <div className="text-white text-xl p-3">
+                    <span>{session.user?.email!.charAt(0).toUpperCase()}</span>
+                  </div>
+                </div>
+              )}
+            </div>
+            <ul
+              tabIndex={0}
+              className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
+            >
+              <li>
+                <p className="text-base-200 tetx-xl">{session.user?.email}</p>
+              </li>
+              <li>
+                <Link href="/api/auth/signout">
+                  <TbLogout className="text-base-200 h-8 w-8" />
+                </Link>
+              </li>
+            </ul>
+          </div>
         )}
         {status === "unauthenticated" && (
           <Link href="/api/auth/signin">

6f91ffc16ef05d8cfb41afbd221016d1733321f4
MErahimi <mohammaderahimi80@gmail.com>
2024-09-13 16:53:32 +0330

    Add info to navbar

--- app/pages/Navbar/NavBarAction.tsx
+++ app/pages/Navbar/NavBarAction.tsx
@@ -3,6 +3,8 @@
 import { useSession } from "next-auth/react";
 import { RiAdminLine } from "react-icons/ri";
 import { TbLogin2, TbLogout } from "react-icons/tb";
+import { FiEdit } from "react-icons/fi";
+import { FaUser } from "react-icons/fa";
 import Link from "next/link";
 import ThemeController from "./ThemeController";
 
@@ -16,19 +18,22 @@ const NavBarAction = () => {
       <ThemeController />
       <div className="">
         {status === "loading" && (
-          <span className="loading loading-spinner loading-md text-white"></span>
+          <span className="loading loading-spinner loading-md text-white mt-2"></span>
         )}
         {status === "authenticated" && (
           <div className="dropdown dropdown-end z-50">
             <div tabIndex={0} role="button">
               {session.user?.image ? (
-                <div className="avatar mt-2">
-                  <div className="ring-primary ring-offset-white w-8 rounded-full ring ring-offset-2">
-                    <img src={session.user.image} />
+                <div className="mt-2">
+                  <div className="ring-primary ring-offset-white w-8 h-8 rounded-full ring ring-offset-2 overflow-hidden">
+                    <img
+                      className="w-8 h-9 rounded-full object-cover"
+                      src={session.user.image}
+                    />
                   </div>
                 </div>
               ) : (
-                <div className="avatar placeholder border-2 bg-[#3b814f] rounded-full flex flex-center">
+                <div className="avatar placeholder border-2 bg-[#3b814f] rounded-full flex flex-center w-9 h-9">
                   <div className="text-white text-xl p-3">
                     <span>{session.user?.email!.charAt(0).toUpperCase()}</span>
                   </div>
@@ -37,14 +42,31 @@ const NavBarAction = () => {
             </div>
             <ul
               tabIndex={0}
-              className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
+              className="dropdown-content menu bg-base-100 rounded-box z-[1] w-72 p-2 shadow"
             >
+              <div className="flex flex-center">
+                <h2 className="text-secondary font-semibold mt-2 mb-4 text-xl border-b-2 border-primary pb-2">
+                  Manage your Account
+                </h2>
+              </div>
               <li>
-                <p className="text-base-200 tetx-xl">{session.user?.email}</p>
+                <div className="flex items-center">
+                  <FaUser className="text-secondary text-xl" />
+                  <h2 className="text-base-200 text-base">
+                    {session.user?.email}
+                  </h2>
+                </div>
+              </li>
+              <li>
+                <Link href="/auth/signup">
+                  <FiEdit className="text-secondary text-xl" />
+                  <h2 className="text-base-200 text-base">Edit</h2>
+                </Link>
               </li>
               <li>
                 <Link href="/api/auth/signout">
-                  <TbLogout className="text-base-200 h-8 w-8" />
+                  <TbLogout className="text-secondary text-2xl" />
+                  <h2 className="text-base-200 text-base"> Sign Out</h2>
                 </Link>
               </li>
             </ul>

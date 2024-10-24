44ec44be8ee843af5cd2a9f9583cf09b0768bc48
MErahimi <mohammaderahimi80@gmail.com>
2024-10-24 13:06:48 +0330

    Change product card skeleton

--- app/product/[Category]/loading.tsx
+++ app/product/[Category]/loading.tsx
@@ -9,12 +9,24 @@ const LoadingCategoryFilterPage = () => {
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
           {Products.map(() => (
             <div>
-              <div className="skeleton w-full h-60 mb-5"></div>
-              <div className="flex flex-col gap-4">
-                <div className="skeleton w-40 h-7"></div>
-                <div className="skeleton w-28 h-5"></div>
-                <div className="skeleton w-20 h-5"></div>
-                <div className="skeleton w-16 h-4"></div>
+              <div className="skeleton w-full h-60 mb-8"></div>
+              <div className="flex flex-col gap-4 p-5">
+                <div className="skeleton w-48 h-7"></div>
+                <div className="flex flex-col gap-2 my-5">
+                  <div className="skeleton w-full h-3"></div>
+                  <div className="skeleton w-60 h-3"></div>
+                </div>
+                <div className="flex items-center justify-between">
+                  <div className="skeleton w-32 h-7"></div>
+                  <div className="skeleton w-20 h-7"></div>
+                </div>
+                <div className="flex gap-4 flex-center justify-between flex-row my-5">
+                  <div className="skeleton w-40 h-12"></div>
+                  <div className="flex gap-3 items-center">
+                    <div className="skeleton w-8 h-8"></div>
+                    <div className="skeleton w-8 h-8"></div>
+                  </div>
+                </div>
               </div>
             </div>
           ))}
